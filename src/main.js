9fb12cff1d235ae2f0682e9af9b0cf417c8052f1
pdminh97 <pdminh97@gmail.com>
2019-08-10 14:14:46 +0700

    Init

--- src/main.js
+++ src/main.js
@@ -2,7 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vuetify from 'vuetify/lib'
+import Vuesax from 'vuesax'
+import 'vuesax/dist/vuesax.css' //Vuesax styles
 
+Vue.use(Vuetify)
+Vue.use(Vuesax)
 Vue.config.productionTip = false
 
 new Vue({
